a3bf10be46d65fc100cd6af609c372bc401eb583
Omar Ali <omar.ali0703@gmail.com>
2021-12-16 11:10:48 +0000

    updates

--- functions.js
+++ functions.js
@@ -2,3 +2,8 @@ $(window).scroll(function(e) {
     var distanceScrolled = $(this).scrollTop();
     $('.container-image').css('-webkit-filter', 'blur('+distanceScrolled/100+'px)');
 });
+
+
+$('.ieee-logo').on('click', function() {
+    window.location='http://wcci2022.org';
+});
