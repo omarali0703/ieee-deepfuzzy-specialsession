adff096027a439d9c804bc46604f261d1e74aef0
Omar Ali <omar.ali0703@gmail.com>
2022-01-22 10:11:44 +0000

    updates

--- functions.js
+++ functions.js
@@ -1,6 +1,7 @@
 $(window).scroll(function(e) {
     var distanceScrolled = $(this).scrollTop();
     $('.container-image').css('-webkit-filter', 'blur('+distanceScrolled/100+'px)');
+    $('.container-image-2').css('-webkit-filter', 'blur('+distanceScrolled/100+'px)');
 });
 
 
